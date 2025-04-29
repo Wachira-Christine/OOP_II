31a40118d7699b7f4d25582c4027a014d55bec5c
Wachira-Christine <mumbi.wachira@strathmore.edu>
2025-04-29 14:01:46 +0300

    adding challenge 3

---
+++ challenge3.java
@@ -0,0 +1,17 @@
+// if else challenge
+public class challenge3 {
+  //performing a number check using if else
+  //Checks if the number is positive, negative or zero
+  public static void main(String[]args){
+    int number = 10;
+    if (number > 0){
+        System.out.println("The number is positive");
+    
+    }else if (number < 0){
+        System.out.println(" The number is negative");
+    } else
+    {
+        System.out.println("The number is zero");
+    }
+  }  
+}
