9cc257cd01b75ee3e763fcf0c9ba2e17bb5976c1
Wachira-Christine <mumbi.wachira@strathmore.edu>
2025-04-29 14:00:49 +0300

    Adding challenge 1

---
+++ challenge1.java
@@ -0,0 +1,12 @@
+// Calling a function
+public class challenge1 {
+    //creating the function
+    void intro(){
+        System.out.println("My name is Christine. I am an ICS student at Strathmore University");
+    }
+
+    public static void main(String [] args){
+        //calling the fuunction
+        (new challenge1()).intro();
+    }
+}
